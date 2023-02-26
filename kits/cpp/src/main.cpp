c5c9fb68bd5d4284174049e3df129f674cf34acf
Zoltán Bartal <bartalzoltan@gmail.com>
2023-02-26 19:25:18 +0100

    make debug env

--- kits/cpp/src/main.cpp
+++ kits/cpp/src/main.cpp
@@ -1,15 +1,49 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
 #include "agent.hpp"
 #include "lux/json.hpp"
 #include "lux/log.hpp"
 
+//#define SAVE_DEBUG
+//#define LOAD_DEBUG
+
+void logInput(std::string fileName, json s) {
+    // declaring character array (+1 for null terminator)
+    char* char_array = new char[fileName.length() + 1];
+
+    // copying the contents of the
+    // string to char array
+    strcpy(char_array, fileName.c_str());
+    lux::dumpJsonToFile(char_array, s);
+}
+
 int main() {
     Agent agent;
+#if defined(SAVE_DEBUG) || defined(LOAD_DEBUG)
+    int turn = 0;
+#endif
     while (std::cin && !std::cin.eof()) {
+#if defined(SAVE_DEBUG) || defined(LOAD_DEBUG)
+        turn++;
+        std::string turnString = std::format("turns/{}_input.json", std::to_string(turn));
+#endif
         json input;
+        
+#ifdef LOAD_DEBUG
+        std::ifstream f(std::format("Debug/{}", turnString));
+        //std::ifstream f("d:/kaggle/Lux2Make/Debug/turns/1_input.json");
+        
+        //std::string a;
+        //    f >> a;
+        f >> input;
+#else
         std::cin >> input;
-
+#endif
+#ifdef SAVE_DEBUG
+        logInput((turnString), input);
+#endif
         lux::dumpJsonToFile("input.json", input);
 
         input.at("step").get_to(agent.step);
